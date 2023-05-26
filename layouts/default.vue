e0fa327928be6260cf4f3352c3a517d779778865
wilker818 <wilkerferreira818@gmail.com>
2023-05-26 11:20:54 -0300

    feat: added new favicon

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,7 +11,7 @@ useHead({
   },
   link: [
     {
-      rel: 'icon', type: 'image/png', href: '/nuxt.png',
+      rel: 'icon', type: 'image/png', href: '/favicon.png',
     },
     {
       rel: 'preconnect',

c9b38e84c779b9ade11a3aa13d39399a16fb0284
wilker818 <wilkerferreira818@gmail.com>
2023-05-31 11:14:53 -0300

    refactor: removed old project and added new updated project

--- app.vue
+++ app.vue
@@ -1,38 +1,5 @@
-<script setup>
-import { useI18n } from 'vue-i18n'
-import { useLocaleStore } from './composables/locale'
-const { locale } = useI18n({ useScope: 'global' })
-const cookieLocale = useLocaleStore()
-
-locale.value = cookieLocale.getLocale || 'pt'
-
-const loadingPage = ref()
-
-onBeforeMount(() => {
-  loadingPage.value = false
-})
-
-onMounted(() => {
-  setTimeout(() => {
-    loadingPage.value = true
-  }, 1000)
-})
-</script>
-
 <template>
-  <LoadingPage v-show="!loadingPage" />
-
-  <NuxtLayout>
-    <NuxtPage transition />
-  </NuxtLayout>
+  <div>
+    <NuxtWelcome />
+  </div>
 </template>
-
-<style>
-.v-enter-active, .v-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.v-enter-from, .v-leave-to {
-  opacity: 0;
-}
-</style>

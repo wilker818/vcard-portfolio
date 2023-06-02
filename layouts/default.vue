2fd0952e5f272221acaa2209bc43df62c993e809
wilker818 <wilkerferreira818@gmail.com>
2023-06-02 10:43:48 -0300

    feat: added SEO

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,42 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { useI18n } from "vue-i18n";
+
 const { locale } = useI18n({ useScope: "global" });
+
+const lang = computed(() => locale.value);
 
 useHead({
   htmlAttrs: {
-    lang: locale,
+    lang: lang.value as string,
   },
   titleTemplate: (pageTitle) => {
     return pageTitle
       ? `${pageTitle} - Wilker Ferreira - Developer`
       : "Wilker Ferreira - Developer";
   },
+  meta: [
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=devide-width, initial-scale=1" },
+    {
+      hid: "keywords",
+      name: "keywords",
+      content:
+        "developer, software enginner, front-end developer, wilker developer, wilker ferreira, wilker front-end, fron-end, desenvolvedor front-end",
+    },
+    {
+      hid: "og:title",
+      property: "og:title",
+      content: "Wilker Ferreira - Developer",
+    },
+    {
+      hid: "og:description",
+      property: "og:description",
+      content:
+        "Descubra meu portfólio de desenvolvimento de sistemas, onde crio soluções funcionais e impactantes. Com experiência em diversas áreas, como desenvolvimento web e aplicações empresariais, ofereço soluções criativas para impulsionar seus projetos. Explore meu trabalho e dê vida às suas ideias com tecnologia.",
+    },
+    { hid: "og:image", property: "og:image", content: "/favicon.png" },
+  ],
   link: [
     {
       rel: "icon",
@@ -34,12 +60,10 @@ useHead({
   script: [
     {
       src: "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js",
-      body: false,
       type: "module",
     },
     {
       src: "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js",
-      body: false,
       nomodule: true,
     },
   ],
@@ -48,6 +72,7 @@ useHead({
 
 <template>
   <main class="relative">
+    u
     <Sidebar />
     <div class="main-content">
       <Navbar />

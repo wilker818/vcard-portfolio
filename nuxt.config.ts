d6f0f18c1ce75bbb63673a8275dfc7faa61dcf55
wilker818 <wilkerferreira818@gmail.com>
2023-06-01 22:56:25 -0300

    feat: added lazyload and

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     "@nuxtjs/color-mode",
     "@nuxtjs/tailwindcss",
     "@intlify/nuxt3",
+    "@nuxt/image-edge",
   ],
   experimental: {
     reactivityTransform: true,

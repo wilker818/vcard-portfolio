efe8f49ae4ed3592d6bca0c1e4d3f2fe64b418cb
wilker818 <wilkerferreira818@gmail.com>
2023-06-01 21:15:41 -0300

    feat(pages): added and configured to typescript

---
+++ pages/contact.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+useHead({
+  title: "Contact",
+});
+</script>
+
+<template>
+  <article class="contact active" data-page="contact">
+    <header>
+      <h2 class="h2 article-title">
+        {{ $t("pages.contact.title") }}
+      </h2>
+    </header>
+
+    <section class="contact-form">
+      <h3 class="h3 form-title">
+        {{ $t("pages.contact.titleContactForm") }}
+      </h3>
+
+      <form action="#" class="form" data-form>
+        <div class="input-wrapper">
+          <input
+            type="text"
+            name="fullname"
+            class="form-input"
+            :placeholder="$t('pages.contact.placeholderFullName')"
+            required
+            data-form-input
+          />
+
+          <input
+            type="email"
+            name="email"
+            class="form-input"
+            :placeholder="$t('pages.contact.placeholderEmailAdress')"
+            required
+            data-form-input
+          />
+        </div>
+
+        <textarea
+          name="message"
+          class="form-input"
+          :placeholder="$t('pages.contact.placeholderYourMessage')"
+          required
+          data-form-input
+        />
+
+        <button class="form-btn" type="submit" disabled data-form-btn>
+          <ion-icon name="paper-plane" />
+          <span> {{ $t("pages.contact.buttonSendMessage") }}</span>
+        </button>
+      </form>
+    </section>
+  </article>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useI18n } from "vue-i18n";
+
+import { useLocaleStore } from "../composables/locale";
+
+const { t } = useI18n({ useScope: "global" });
+
+useHead({});
+
+onMounted(() => {
+  useLocaleStore();
+});
+</script>
+
+<template>
+  <article class="about active" data-page="about">
+    <header>
+      <h2 class="h2 article-title">
+        {{ t("pages.index.title") }}
+      </h2>
+    </header>
+
+    <section class="about-text">
+      <p>
+        {{ t("pages.index.textAbout") }}
+      </p>
+
+      <p>
+        {{ t("pages.index.textAbout2") }}
+      </p>
+      <p>
+        {{ t("pages.index.textAbout3") }}
+      </p>
+      <p>
+        {{ t("pages.index.textAbout4") }}
+      </p>
+      <p>
+        {{ t("pages.index.textAbout5") }}
+      </p>
+      <p>
+        {{ t("pages.index.textAbout6") }}
+      </p>
+      <p>
+        {{ t("pages.index.textAbout7") }}
+      </p>
+    </section>
+  </article>
+</template>
